4a4c2c60079e8386867c80411dc139bbba9e9363
jdlovin7185 <jdlovin7185@gmail.com>
2024-10-08 19:55:35 -0400

    refactored the variable Title to title

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct DailyScrum {
-    var Title: String
+    var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
@@ -17,10 +17,10 @@ struct DailyScrum {
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
-        DailyScrum(Title: "Design", 
+        DailyScrum(title: "Design", 
                    attendees: ["Petty Pablo", "Lil Jon", "Eastside Boyz"],
                    lengthInMinutes: 20, theme: .yellow),
-        DailyScrum(Title: "App Dev", attendees: ["Lil Wayne", "Birdman", "Future"], lengthInMinutes: 10, theme: .orange),
-        DailyScrum(Title: "Web Dev", attendees: ["Fred Again...", "Skrillex", "Diplo"], lengthInMinutes: 5, theme: .poppy)
+        DailyScrum(title: "App Dev", attendees: ["Lil Wayne", "Birdman", "Future"], lengthInMinutes: 10, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Fred Again...", "Skrillex", "Diplo"], lengthInMinutes: 5, theme: .poppy)
     ]
 }
