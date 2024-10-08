10b552cf2514a32374c81d66b7de7bbc6b0fa965
jdlovin7185 <jdlovin7185@gmail.com>
2024-10-08 19:27:42 -0400

    first commit

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct DailyScrum {
+    var Title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(Title: "Design", 
+                   attendees: ["Petty Pablo", "Lil Jon", "Eastside Boyz"],
+                   lengthInMinutes: 20, theme: .yellow),
+        DailyScrum(Title: "App Dev", attendees: ["Lil Wayne", "Birdman", "Future"], lengthInMinutes: 10, theme: .orange),
+        DailyScrum(Title: "Web Dev", attendees: ["Fred Again...", "Skrillex", "Diplo"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
